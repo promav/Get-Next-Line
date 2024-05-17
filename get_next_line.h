ae21ccd881213d8053786fa15a75a5c30bbe3d2c
Pablo Romero Avila <pabromer@c3r1s1.42malaga.com>
2024-05-17 13:44:12 +0200

    Get Next Line Start

---
+++ get_next_line.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pabromer <pabromer@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/17 13:27:00 by pabromer          #+#    #+#             */
+/*   Updated: 2024/05/17 13:34:51 by pabromer         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdio.h>
+# include <fcntl.h>
+
+char *get_next_line(int fd);
+
+#endif 
