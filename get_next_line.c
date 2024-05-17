3b7d7a93fe5422add5a2e26021274b986c96bc45
Pablo Romero Avila <pabromer@c3r1s1.42malaga.com>
2024-05-17 13:50:19 +0200

    Get Next Line Start

--- get_next_line.c
+++ get_next_line.c
@@ -6,13 +6,16 @@
 /*   By: pabromer <pabromer@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/17 13:27:06 by pabromer          #+#    #+#             */
-/*   Updated: 2024/05/17 13:35:46 by pabromer         ###   ########.fr       */
+/*   Updated: 2024/05/17 13:49:59 by pabromer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char *get_next_line(int fd)
+char	*get_next_line(int fd)
 {
-	size_t 
+	size_t	nbytes;
+	char	buffer[1024];
+
+	nbytes = read(fd, buffer, )
 }
